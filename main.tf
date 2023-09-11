67137150a582613ea462e7e0a30a858d36be29dd
Demir Senturk <demirsenturk@microsoft.com>
2023-09-11 16:23:53 +0200

    Dual-stack Subnet design

--- main.tf
+++ main.tf
@@ -1,14 +1,34 @@
-Resource "azurerm_virtual_network" "mainnetwork" {
-  name = "mainnetwork"
-  location = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  address_space = ["10.0.0.0/16"]
+#Create the VNET 
+resource "azurerm_virtual_network" "mainnetwork-dualstack" {
+    name                = var.vnet_name
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    address_space       = var.vnet_address_space
 }
 
-resource "azurerm_subnet" "test_subnet" {
+resource "azurerm_subnet" "test_subnets" {
     name = "${lookup(element(var.subnet_prefix, count.index), "name")}"
     count = "${length(var.subnet_prefix)}"
     resource_group_name = var.resource_group_name
     virtual_network_name = var.vnet_name
-    address_prefix = "${lookup(element(var.subnet_prefix, count.index), "ip")}"
+    address_prefixes = "${lookup(element(var.subnet_prefix, count.index), "ip")}"
+}
+
+#Create the NSG
+resource "azurerm_network_security_group" "IPv6-nsg" {
+  name                = "IPv6-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+  name                       = "Port_80"
+  priority                   = 100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix    = "*"
+  destination_address_prefix = "*"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -2,36 +2,55 @@ variable "subnet_prefix" {
   type = list
   default = [
     {
-      ip      = "10.0.1.0/24"
+      ip      = ["10.0.1.0/24","fd00:db8:deca:daea::/64"] # "10.0.1.0/24" for IPv4 only
       name     = "subnet-1"
     },
     {
-      ip      = "10.0.2.0/24"
+      ip      = ["10.0.2.0/24","fd00:db8:deca:daeb::/64"]
       name     = "subnet-2"
     },
     {
-      ip      = "10.0.3.0/24"
+      ip      = ["10.0.3.0/24","fd00:db8:deca:daec::/64"]
       name     = "subnet-3"
     },
     {
-      ip      = "10.0.4.0/24"
+      ip      = ["10.0.4.0/24","fd00:db8:deca:daed::/64"]
       name     = "subnet-4"
     },
     {
-      ip      = "10.0.5.0/24"
+      ip      = ["10.0.5.0/24","fd00:db8:deca:daee::/64"]
       name     = "subnet-5"
     }
    ]
 }
 
+#Resource Group Name
 variable "resource_group_name" {
   type        = string
-  default     = "testProv"
+  default     = "rsg-domain_controller"
   description = "name of the resource group."
+}
+
+#Location 
+variable "location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created. Please review the Hosting Standard to select a approved location"
+  default     = "NorthEurope" # Azure Region https://learn.microsoft.com/en-us/dotnet/api/azure.core.azurelocation?view=azure-dotnet
 }
 
 variable "vnet_name" {
   type        = string
-  default     = "prov_labvnet"
+  default     = "mainnetwork-dualstack"
   description = "name of the virtual network."
+}
+
+#Overall IPv4 and IPv6 Space for the VNET
+variable "vnet_address_space" {
+  description = "The address space of the VNET"
+  default = ["10.0.0.0/16","fd00:db8:deca::/48"] # IPv4, IPv6 Space
+}
+
+#IPv4 and ipV6 Space allocated for Subnet 
+variable "IPv6-Subnet" {
+  description = "The subnet to be used"
+  default = ["10.0.0.0/24","fd00:db8:deca:daed::/64"] # IPv4, IPv6 Space
 }
